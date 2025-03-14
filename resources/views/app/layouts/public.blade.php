3a49fcd68e51e2249412f1d1f05814a23aea8251
Muhammad Yunus <muhammadyunus1072@gmail.com>
2025-03-14 17:36:08 +0700

    fix button signIn

--- resources/views/app/layouts/public.blade.php
+++ resources/views/app/layouts/public.blade.php
@@ -99,7 +99,61 @@
                           </div>
                        </div>
                        <div class="flex-equal text-end ms-1">
-                          <a href="{{ route('login')}}" id="btn-login" class="btn btn-success">Sign In</a>
+                           @if(auth()->check())
+                              <!--begin::User menu-->
+                              <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
+                                 <!--begin::Menu wrapper-->
+                                 <div class="cursor-pointer symbol symbol-35px btn btn-success"
+                                    data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent"
+                                    data-kt-menu-placement="bottom-end">
+                                    My Account
+                                 </div>
+                                 <!--begin::User account menu-->
+                                 <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
+                                    data-kt-menu="true">
+                                    <!--begin::Menu item-->
+                                    <div class="menu-item px-3" style="overflow:scroll;">
+                                       <div class="menu-content d-flex align-items-center px-3">
+                                             <!--begin::Username-->
+                                             <div class="d-flex flex-column">
+                                                <div class="fw-bold d-flex align-items-center fs-5">
+                                                   {{ Auth::user()->name }}
+                                                </div>
+                                                <a href="#"
+                                                   class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
+                                             </div>
+                                             <!--end::Username-->
+                                       </div>
+                                    </div>
+                                    <!--end::Menu item-->
+
+                                    <!--begin::Menu item-->
+                                    <div class="menu-item px-5 d-none unhide-123">
+                                    </div>
+                                    <!--end::Menu item-->
+
+                                    <!--begin::Menu item-->
+                                    @if (config('template.profile_route'))
+                                       <div class="menu-item px-5">
+                                             <a href="{{ route(config('template.profile_route')) }}"
+                                                class="menu-link px-5">Profil</a>
+                                       </div>
+                                    @endif
+                                    <!--end::Menu item-->
+
+                                    <!--begin::Menu item-->
+                                    <div class="menu-item px-5">
+                                       <a href="{{ route('logout') }}" class="menu-link px-5">Sign Out</a>
+                                    </div>
+                                    <!--end::Menu item-->
+                                 </div>
+                                 <!--end::User account menu-->
+                                 <!--end::Menu wrapper-->
+                              </div>
+                              <!--end::User menu-->
+                           @else
+                                 <a href="{{ route('login')}}" id="btn-login" class="btn btn-success">Sign In</a>
+                           @endif
                        </div>
                     </div>
                  </div>
