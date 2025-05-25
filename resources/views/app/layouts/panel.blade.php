f188c8b18966d7d7f85c1d282869ef8f815533f5
Muhammad Yunus <muhammadyunus1072@gmail.com>
2025-05-25 16:29:13 +0700

    asd

--- resources/views/app/layouts/panel.blade.php
+++ resources/views/app/layouts/panel.blade.php
@@ -27,13 +27,13 @@
 
     @stack('css')
     <style>
-        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item .menu-link .menu-title {
+        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item.here>.menu-link .menu-title {
             color: #5d2fc2;
         }
-        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item .menu-link .menu-icon{
+        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item.here>.menu-link .menu-icon{
             color: #5d2fc2;
         }
-        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item .menu-link .menu-arrow{
+        [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item.here>.menu-link .menu-arrow{
             color: #5d2fc2;
         }
         [data-kt-app-layout=dark-sidebar] .app-sidebar .menu>.menu-item .menu-link.active {
