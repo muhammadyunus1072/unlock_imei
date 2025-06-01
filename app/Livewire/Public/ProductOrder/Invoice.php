6cbff19fd093ad2106987011a88183bec635113b
Muhammad Yunus <muhammadyunus1072@gmail.com>
2025-06-01 16:21:41 +0700

    asd

--- app/Livewire/Public/ProductOrder/Invoice.php
+++ app/Livewire/Public/ProductOrder/Invoice.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Public\ProductOrder;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Crypt;
+use App\Models\MasterData\PaymentMethod;
+use App\Models\MasterData\Voucher;
 use App\Repositories\Transaction\Transaction\TransactionRepository;
 
 class Invoice extends Component
@@ -26,7 +28,23 @@ class Invoice extends Component
 
     private function calculatedTotal()
     {
-        $this->subtotal = count($this->transaction->transactionDetails) * $this->transaction->transactionDetails[0]->price;
+        $this->subtotal = count($this->transaction->transactionDetails) * $this->transaction->transactionDetails[0]->product_price;
+        // ADMIN FEE
+        if($this->transaction->payment_method_fee_type === PaymentMethod::TYPE_PERCENTAGE)
+        {
+        $this->admin_fee = calculatedAdminFee($this->subtotal, $this->transaction->payment_method_fee_amount);
+        }else{
+        $this->admin_fee = $this->transaction->payment_method_fee_amount;
+        }
+        
+        // DISCOUNT
+        if($this->transaction->voucher_type === Voucher::TYPE_PERCENTAGE)
+        {
+        $this->discount = calculatedAdminFee($this->subtotal, $this->transaction->voucher_amount);
+        }else{
+        $this->discount = $this->transaction->voucher_amount;
+        }
+        
         $this->grand_total = $this->subtotal + $this->admin_fee - $this->discount;
     }
 
